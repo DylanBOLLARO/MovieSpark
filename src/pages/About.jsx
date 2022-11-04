54664fb35d992f4a7f556912325a3e41c0bf6007
Dylan BOLLARO <105074286+DylanBOLLARO@users.noreply.github.com>
2022-11-04 17:41:13 +0100

    Add files via upload

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+
+function About() {
+  return (
+    <>
+      <Header />
+      <h1>je suis la page About</h1>
+    </>
+  );
+}
+
+export default About;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <h1>je suis la page Home</h1>
+    </>
+  );
+}
+
+export default Home;
